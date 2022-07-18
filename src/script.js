b37cb2a19a2afffe145236d3b536f1eb1eefd0a8
VBasko <106957194+VBasko@users.noreply.github.com>
2022-07-18 04:41:12 +0400

    Added API call for a forecast

--- src/script.js
+++ src/script.js
@@ -115,7 +115,8 @@ let iconElement = document.querySelector("#weather-icon");
 
 axios.get(apiUrl).then(displayWeather);
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastArea = document.querySelector("#forecast-area");
 
   let forecastCard = "";
@@ -140,11 +141,9 @@ function displayForecast() {
   forecastArea.innerHTML = forecastCard;
 }
 
-displayForecast();
-
 function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeather(response) {
